3fbbdbfe8f98f6f57cb9168fed517194b664c6dd
Patrick Lybarger <lybargerp2000@yahoo.com>
2020-02-14 11:55:44 -0600

    Working on Weather class.

---
+++ LemonadeStand_Pat/Weather.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand_Pat
+{
+    class Weather
+    {
+        string condition;
+        int temperature;
+        List<string> weatherConditions;
+
+        public Weather()
+        {
+            weatherConditions = new List<string>();
+            return;
+            condition = "sunny";
+            temperature = 65;
+        }
+
+        public void AddWeatherCondition()
+        {
+            weatherConditions.Add("cloudy");
+            weatherConditions.Add("rainy");
+            weatherConditions.Add("sunny");
+           
+
+        }
+    }
+}
